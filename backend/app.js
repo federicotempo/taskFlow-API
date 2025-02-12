17098a857e186c2f27666dac5b8f303358247c0e
federicotempo <federicotempo98@gmail.com>
2025-02-12 14:39:47 -0300

    Add middleware
    -cors
    -helmet
    -limiter
    -json()

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({ windowMs: 60 * 1000, max: 100 });
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send("Hello world");
