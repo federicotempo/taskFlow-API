a208092bb1335833c9d914ded71280ac48fefdba
Federico Tempo <136754690+federicotempo@users.noreply.github.com>
2025-02-12 14:56:31 -0300

    Backend config routes app (#4)
    * install dotenv
    
    * Create prisma schema
    
    * Add middleware
    
    -cors
    -helmet
    -limiter
    -json()
    
    * Add routes on app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({ windowMs: 60 * 1000, max: 100 });
 
-app.get("/", (req, res) => {
-  return res.send("Hello world");
-});
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+
+const authRoutes = require("./src/routes/authRoutes");
+const taskRoutes = require("./src/routes/taskRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/task", taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`TaskFlow is running on http://localhost:${PORT}`);
