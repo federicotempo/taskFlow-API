ade3638cabcf76a7bdc77973f45529e76c71cd8b
Federico Tempo <federicotempo98@gmail.com>
2025-03-04 10:56:40 -0300

    Update App.tsx

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -2,19 +2,10 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 
 const App: React.FC = () => {
-  const tasks = [
-    {
-      title: "Task 1",
-      description: "This is the description of Task 1.",
-      status: "Completed",
-      dueDate: "2025-03-10",
-    },
-  ];
-
   return (
     <div className="App">
       <Header userName="Federico"></Header>
-      <MainContent tasks={tasks}></MainContent>
+      <MainContent></MainContent>
     </div>
   );
 };
